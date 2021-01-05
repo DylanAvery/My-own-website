d9367f97cc7d78a9a7cfc57b347a43a00f857fd1
DylanAvery <dylavery712@gmail.com>
2021-01-05 14:41:47 -0500

    SRC units added

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -18,7 +18,7 @@
  * BROWSER POLYFILLS
  */
 
-/** IE11 requires the following for NgClass support on SVG elements */
+/** IE10 and IE11 requires the following for NgClass support on SVG elements */
 // import 'classlist.js';  // Run `npm install --save classlist.js`.
 
 /**
@@ -67,5 +67,5 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
 /*
 Copyright Google LLC. All Rights Reserved.
 Use of this source code is governed by an MIT-style license that
-can be found in the LICENSE file at https://angular.io/license
+can be found in the LICENSE file at http://angular.io/license
 */
